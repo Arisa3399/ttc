607cef42c6fbf385640a5da5943505fbeb2b01a3
Yugeta.Koji <geta1972@gmail.com>
2023-12-03 20:11:23 +0900

    request: file-upload

--- public/page/request/js/main.js
+++ public/page/request/js/main.js
@@ -1,8 +1,67 @@
+import { Uuid } from "../../../asset/js/uuid.js"
+
 (function(){
   function Main(){
     window.submitted = false;
     document.querySelector(`button[name='post']`).addEventListener('click' , click_button)
+    set_event()
+    set_form()
     set_iframe()
+  }
+
+  function set_event(){
+    const input_file = document.querySelector(`input[name="file[]"]`)
+    if(input_file){
+      input_file.addEventListener("change" , change_input_file)
+    }
+  }
+
+  function change_input_file(e){
+    const preview = document.querySelector(`.preview`)
+    if(!preview){return}
+    const uuid = new Uuid().id
+    preview.innerHTML = ""
+    for(let i=0; i<e.target.files.length; i++){
+      const file = e.target.files[i]
+      const ext = get_ext(file.name)
+      const id = `${uuid}-${i}`
+      switch(ext){
+        case "png":
+        case "jpg":
+        case "jpeg":
+        case "gif":
+        case "webp":
+          const div = document.createElement("div")
+          div.setAttribute("data-uuid" , id)
+          const img = new Image()
+          const url = URL.createObjectURL(file)
+          img.src = url
+          div.appendChild(img)
+          const name = document.createElement("div")
+          name.className = "name"
+          name.textContent = file.name
+          div.appendChild(name)
+          preview.appendChild(div)
+          break
+        case "pdf":
+          break
+      }
+    }
+  }
+
+  function get_ext(filename){
+    const sp = filename.split(".")
+    const ext = sp.slice(-1).join("")
+    return ext ? ext.toLowerCase() : null
+  }
+
+  function set_form(){
+    const form = document.querySelector(`form[name="contact"]`)
+    if(!form){return}
+    form.action = "https://docs.google.com/forms/d/e/1FAIpQLSflfJ7DaBmSbE-kZZXuUEtre2r1CIdgWtabUzW3BFlrhijgYQ/formResponse"
+    form.target = "complete"
+    // form.addEventListener("submit" , (()=>{window.submitted=true}))
+    // form.onsubmit = function(){return false}
   }
 
   function set_iframe(){
@@ -35,7 +94,45 @@
     alert(message)
     document.querySelector('.error').textContent = message
   }
+
   function submit(){
+    // console.log("submit")
+    file_upload()
+    // window.submitted = true
+    // document.forms.contact.submit()
+  }
+
+  function file_upload(){
+    console.log("file_upload")
+    const formData = new FormData(document.forms.contact)
+    // var files = document.querySelector(`input[type='file'][name='file']`).files;
+    // for (let i=0; i<files.length; i++) {
+    //   formData.append("file[]", files[i]);
+    // }
+    const req = new XMLHttpRequest();
+    req.open("POST", "upload.php" , true);
+    req.onload = file_uploaded
+    req.send(formData);
+  }
+
+  // function google_form_send(){
+  //   document.forms.contact.action = "https://docs.google.com/forms/d/e/1FAIpQLSflfJ7DaBmSbE-kZZXuUEtre2r1CIdgWtabUzW3BFlrhijgYQ/formResponse"
+  // }
+
+  function file_uploaded(e){
+    if(!e || !e.target || !e.target.response){return}
+    const res = JSON.parse(e.target.response)
+    if(!res || res.status !== "success"){return}
+    let value = ""
+    for(let i=0; i<res.datas.length; i++){
+      const data = res.datas[i]
+      const path = location.pathname.split("/").slice(0,-1).join("/")
+      const url = `${location.protocol}://${location.host}${path}/${data.path}`
+      value += `# ${i} : ${data.base}\n${url}\n\n`
+    }
+    document.querySelector(`textarea[data-name="files"]`).value = value
+
+    // google-form-submit
     window.submitted = true
     document.forms.contact.submit()
   }
